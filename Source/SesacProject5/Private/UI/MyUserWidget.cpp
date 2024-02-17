fd3b9c965786175d1b599256210f6b4ce2552dc2
myeonghoon <mhoo999@naver.com>
2024-02-17 14:14:45 +0900

    YMH:0217_UI 작업
    title, lobby, gameLoading 화면 작업
    + 게임 세션

--- Source/SesacProject5/Private/UI/MyUserWidget.cpp
+++ Source/SesacProject5/Private/UI/MyUserWidget.cpp
@@ -3,9 +3,58 @@
 
 #include "UI/MyUserWidget.h"
 
+#include "Components/Button.h"
 #include "Components/WidgetSwitcher.h"
 
-void UMyUserWidget::gotoLobby()
+void UMyUserWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	// ---------------- title ---------------------
+	btn_gameStart->OnClicked.AddDynamic(this, &UMyUserWidget::MovetoLobby);
+	btn_exit->OnClicked.AddDynamic(this, &UMyUserWidget::ExitGame);
+
+	// ---------------- Lobby ---------------------
+	btn_Inventory->OnClicked.AddDynamic(this, &UMyUserWidget::OpenInventory);
+	btn_city->OnClicked.AddDynamic(this, &UMyUserWidget::OpenCityTab);
+	btn_movetoField->OnClicked.AddDynamic(this, &UMyUserWidget::MovetoField);
+	btn_workBench->OnClicked.AddDynamic(this, &UMyUserWidget::OpenWorkBenchTab);
+	btn_back->OnClicked.AddDynamic(this, &UMyUserWidget::MovetoTitle);
+
+	
+}
+
+void UMyUserWidget::MovetoLobby()
 {
 	widgetSwitcher->SetActiveWidgetIndex(1);
+}
+
+void UMyUserWidget::ExitGame()
+{
+	FGenericPlatformMisc::RequestExit(true);
+}
+
+void UMyUserWidget::OpenInventory()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Open Inventory!"));
+}
+
+void UMyUserWidget::OpenCityTab()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Open City!"));
+}
+
+void UMyUserWidget::MovetoField()
+{
+	widgetSwitcher->SetActiveWidgetIndex(2);
+}
+
+void UMyUserWidget::OpenWorkBenchTab()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Open WorkBench!"));
+}
+
+void UMyUserWidget::MovetoTitle()
+{
+	widgetSwitcher->SetActiveWidgetIndex(0);
 }
